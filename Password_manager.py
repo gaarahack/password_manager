61a25f20e8bed9aa5975e9ba74853091e942fddd
rahul <111223495+gaarahack@users.noreply.github.com>
2023-01-31 01:08:03 +0530

    Update Password_manager.py

--- Password_manager.py
+++ Password_manager.py
@@ -1,36 +1,35 @@
 import getpass
 
-passwords ={}
+passwords = {}
 
-def create_passwords(website,password):
+def create_password(website,password):
     passwords[website] = password
-    print(f"Password for {website} is created succefully")
+    print(f"Password created successfully for {website}")
 
 def get_password(website):
     try:
-        return passwords[website]    
+        return passwords[website]
     except KeyError:
-        return "Password for this website is not found"
+        return "Can't find the password for the given website"
 
-running = True
+running =True
 
 while running:
-    print("Welcome to the password manager")
-    print("1. create a password for a website")
-    print("2. Know your created password")
-    print("3. Quit")
+    print("1 Create a password for the website")
+    print('2. Get password which are already stored')
+    print("3 Quit")
 
-    choice = int(input("Enter the option: "))
+    choice =int(input("Enter any option: "))
 
     if choice == 1:
         website = input("Enter the name of the website: ")
-        password = input("Enter the password for the website: ")
-        create_passwords(website,password)
+        password = getpass.getpass("Enter the password: ")
+        create_password(website,password)
 
     if choice == 2:
         website = input("Enter the name of the website: ")
         print(f"Password: {get_password(website)}")
 
     if choice == 3:
-        running =False
-        exit()
+        running = False
+        exit()    
